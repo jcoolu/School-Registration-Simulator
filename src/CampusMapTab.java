e6ee186774908b509433d4e5e7675d0aecda451a
jcoolu <45079606+jcoolu@users.noreply.github.com>
2018-11-30 23:04:02 -0500

    Update CampusMapTab.java

--- src/CampusMapTab.java
+++ src/CampusMapTab.java
@@ -1,18 +1,16 @@
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
 public class CampusMapTab extends JLabel {
+	
+	public CampusMapTab() {
+		
+	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Image img = new ImageIcon("CampusMapRIC.png").getImage();
 		g.drawImage(img,0, 0, this.getWidth(), this.getHeight(), null);
 	}
-	}
-	
+}
