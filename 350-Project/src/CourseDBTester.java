0c7690022fa541f3fe5d3162dffe18729b7d2390
Kirby Forsberg <kirby.forsberg@hotmail.com>
2018-12-05 15:24:00 -0500

    Add files via upload

---
+++ 350-Project/src/CourseDBTester.java
@@ -0,0 +1,7 @@
+import java.io.FileNotFoundException;
+
+public class CourseDBTester {
+	public static void main(String [] args) throws FileNotFoundException{
+
+	}
+}
--- 350-Project/src/CourseDatabase.java
+++ 350-Project/src/CourseDatabase.java
@@ -12,23 +12,31 @@ import java.util.Scanner;
  * 
  * It is also responsible for writing newly created
  * Course objects back to the text file.
- * 
+ * Uses singleton design pattern to instantiate object.
  * @author Kirby
  * 
  */
 
 public class CourseDatabase {
-	ArrayList<Course> courseDB = new ArrayList<Course>();
+	
+	ArrayList<Course> courseDB;
+
+	public CourseDatabase(){
+		courseDB = new ArrayList<Course>();
+	}
 	
 	/**
 	 * @param file The text file containaing the database
 	 * @return ArrayList containing data from .txt file
 	 * @throws FileNotFoundException
 	 */
-	ArrayList<Course> createArrayList(String file) throws FileNotFoundException{
+	public ArrayList<Course> createArrayList(String file) throws FileNotFoundException{
+		int i = 0;
 		Scanner s = new Scanner(new File(file));
+		s.useDelimiter(",");
 		while (s.hasNext()){
-		    //courseDB.add(s.next()); //FIX
+			
+			i++;
 		}
 		s.close();
 		return courseDB;
@@ -39,14 +47,14 @@ public class CourseDatabase {
 	 * @param courseDB The ArrayList containing data from .txt file originally
 	 * @throws IOException
 	 */
-	void createFile(String file, ArrayList<Course> courseDB) throws IOException{
+	public void createFile(String file, ArrayList<Course> courseDB) throws IOException{
         FileWriter writer = new FileWriter(file);
         int size = courseDB.size();
         for (int i=0;i<size;i++) {
-            String str = courseDB.get(i).toString();
+            String str = courseDB.get(i).toString() + ",";
             writer.write(str);
-            if(i < size-1)	/*This prevent creating a blank like at the end of the file*/
-                writer.write("\n");
+//            if(i < size-1)	/*This prevent creating a blank like at the end of the file*/
+//                writer.write("\n");
         }
         writer.close();
     }
