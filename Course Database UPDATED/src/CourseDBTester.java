dc20dac7331ad7dfb90390e471cc8d4546c6dd9f
jcoolu <45079606+jcoolu@users.noreply.github.com>
2018-12-09 21:30:47 -0500

    Updated Course Database

---
+++ Course Database UPDATED/src/CourseDBTester.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class CourseDBTester {
+	public static void main(String [] args) throws IOException{
+		CourseDatabase courseDataBase = CourseDatabase.getCourses();
+		
+		/*Create Arraylist from given text file*/
+		courseDataBase.createArrayList("CourseDatabase.txt");
+		/*Creates the file from the given arraylist*/
+		courseDataBase.createFile("CourseDatabase.txt");
+	}
+}
