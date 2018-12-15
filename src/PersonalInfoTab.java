dc9c20af33f31a966f96e3752b20cd33c6c2e57a
jcoolu <45079606+jcoolu@users.noreply.github.com>
2018-12-14 20:25:03 -0500

    Update PersonalInfoTab.java

--- src/PersonalInfoTab.java
+++ src/PersonalInfoTab.java
@@ -2,19 +2,40 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 
 public class PersonalInfoTab extends JPanel implements ActionListener {
 	private String[] textFieldLabels = { "\nFirst Name: ", "\nLast Name: ", "Street Address: ", "City: ", "State: ",
-			"Zip Code: ", "Major: ", "Minor: ", "GPA: ", "Student ID: " };
+			"Zip Code: ", "Phone Number: ", "Major: ", "Minor: ", "GPA: ", "Student ID: " };
 	private String[] textFieldsTips = { "Enter First Name.", "Enter Last Name.",
 			"Enter Street Address EX: 123 Smith Street ", "Enter City.", "Enter State. (Initials)", "Enter Zip Code.",
-			"Enter Major(s).", "Enter Minor(s)", "Enter GPA", "Enter Student ID" };
+			"Enter Phone Number.", "Enter Major(s).", "Enter Minor(s)", "Enter GPA", "Enter Student ID" };
 	private static JTextField[] texts; // text fields for personal info
 	private static JPanel[] panels; // panels for personal info
+	private static int studentNumber; // student # (what student you're showing)
 
 	public PersonalInfoTab() {
+		String student = LoginPanel.getUsername();
+		System.out.println(StudentDatabase.getStudent(1).getFirstName());
+		if(student.equals("jcoulombe_0001")) {
+			studentNumber = 1;
+		}
+		if(student.equals("ofelizlima_0002")) {
+			studentNumber = 2;
+		}
+		if(student.equals("kfosberg_0003")) {
+			studentNumber = 3;
+		}
+		if(student.equals("gcastigleigo_0004")) {
+			studentNumber = 4;
+		}
+		if(student.equals("mgarcia_0005")) {
+			studentNumber = 5;
+		}
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		// Adds panel for each textField
 		panels = new JPanel[textFieldLabels.length];
 		texts = new JTextField[panels.length];
@@ -24,7 +45,7 @@ public class PersonalInfoTab extends JPanel implements ActionListener {
 		JPanel name = new JPanel();
 		JPanel location = new JPanel();
 		JPanel degree = new JPanel();
-		JPanel student = new JPanel();
+		JPanel student1 = new JPanel();
 
 		// adding text fields
 		for (int i = 0; i < textFieldLabels.length; i++) {
@@ -44,20 +65,32 @@ public class PersonalInfoTab extends JPanel implements ActionListener {
 				text.setColumns(10);
 				location.add(panels[i]);
 			}
-			if (i == 6 || i == 7) {
+			if (i == 6 || i == 7 || i == 8) {
 				text.setColumns(25);
 				degree.add(panels[i]);
 			}
-			if (i == 8 || i == 9) {
+			if (i == 9 || i == 10) {
 				text.setColumns(15);
-				student.add(panels[i]);
+				student1.add(panels[i]);
 			}
 		}
 		add(name);
 		add(panels[2]);
 		add(location);
 		add(degree);
-		add(student);
+		add(student1);
+
+		texts[0].setText(StudentDatabase.getStudent(studentNumber).getFirstName());
+		texts[1].setText(StudentDatabase.getStudent(studentNumber).getLastName());
+		texts[2].setText(StudentDatabase.getStudent(studentNumber).getAddress());
+		texts[3].setText(StudentDatabase.getStudent(studentNumber).getCity());
+		texts[4].setText(StudentDatabase.getStudent(studentNumber).getState());
+		texts[5].setText(StudentDatabase.getStudent(studentNumber).getZip());
+		texts[6].setText(StudentDatabase.getStudent(studentNumber).getNumber());
+		texts[7].setText(StudentDatabase.getStudent(studentNumber).getMajor());
+		texts[8].setText(StudentDatabase.getStudent(studentNumber).getMinor());
+		texts[9].setText(StudentDatabase.getStudent(studentNumber).getGpa());
+		texts[10].setText(StudentDatabase.getStudent(studentNumber).getId());
 
 		JPanel buttonPanel = new JPanel();
 		setLayout(new FlowLayout());
@@ -76,7 +109,7 @@ public class PersonalInfoTab extends JPanel implements ActionListener {
 	public static ActionListener editInfo() {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				for (int i = 0; i < texts.length - 2; i++) {
+				for (int i = 2; i < texts.length - 2; i++) {
 					texts[i].setEditable(true);
 				}
 			}
@@ -88,7 +121,22 @@ public class PersonalInfoTab extends JPanel implements ActionListener {
 		return new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				for (int i = 0; i < texts.length; i++) {
+					StudentDatabase.getStudent(studentNumber).setFirstName(texts[0].getText());
+					StudentDatabase.getStudent(studentNumber).setLastName(texts[1].getText());
+					StudentDatabase.getStudent(studentNumber).setAddress(texts[2].getText());
+					StudentDatabase.getStudent(studentNumber).setZip(texts[3].getText());
+					StudentDatabase.getStudent(studentNumber).setNumber(texts[4].getText());
+					StudentDatabase.getStudent(studentNumber).setMajor(texts[5].getText());
+					StudentDatabase.getStudent(studentNumber).setMinor(texts[6].getText());
+					StudentDatabase.getStudent(studentNumber).setGpa(texts[7].getText());
+					StudentDatabase.getStudent(studentNumber).setId(texts[8].getText());
 					texts[i].setEditable(false);
+					try {
+						StudentDatabase.createFile("studentDatabase.txt");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		};
